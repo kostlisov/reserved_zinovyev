087dbd51b78de183e80858307ab0fa03115065cf
kostlisov <117742861+kostlisov@users.noreply.github.com>
2023-05-19 18:08:09 +0300

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(mySprite, 100, 100)
+})
+function location () {
+    music.play(music.createSong(assets.song`mySong`), music.PlaybackMode.LoopingInBackground)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite.setPosition(10, 102)
+}
 sprites.onCreated(SpriteKind.Player, function (sprite) {
 	
 })
+let mySprite: Sprite = null
+location()
+mySprite = sprites.create(assets.image`main sprite`, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`player`,
+200,
+true
+)
